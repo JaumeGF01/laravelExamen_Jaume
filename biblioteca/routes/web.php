6b250dd272b498dd7f866f5b96b8aaef87fbaf48
JaumeGF01 <jaugarfra@alu.edu.gva.es>
2025-02-20 19:51:53 +0100

    a

--- biblioteca/routes/web.php
+++ biblioteca/routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
 // Route::get("libros",[LibroController::class, "libros.index"]);
 // Route::get("libros/{id}",[LibroController::class, "libros.show"]);
-Route::resource("/libros",LibroController::class)->only(["index"]);
+Route::resource("/libros",LibroController::class);
 Route::get("login",[LoginController::class, "loginForm"])->name("login");
 Route::post("login", [LoginController::class, "login"]);
 Route::get("logout",[LoginController::class, "logout"])->name("logout");
